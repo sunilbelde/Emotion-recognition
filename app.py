28112b5228b4eb0c927f8ba278661e69ff481c20
sunilbelde <47601858+sunilbelde@users.noreply.github.com>
2021-06-01 17:46:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
 import streamlit as st
 import numpy as np    
 import tensorflow as tf
-import sounddevice as sd
-from scipy.io.wavfile import write
 import librosa # to extract speech features
+import pyaudio
+import wave
 
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 3.5
+WAVE_OUTPUT_FILENAME = "input.wav"
+
+p = pyaudio.PyAudio()
+
+
+def record_audio():
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    print("* recording")
+
+    frames = []
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)
+        frames.append(data)
+
+    print("* done recording")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
 def main():
     #print(cv2.__version__)
     selected_box = st.sidebar.selectbox(
@@ -16,9 +54,6 @@ def main():
         st.sidebar.success('To try by yourself select "Evaluate the model".')
         application()
 
-
-
-    
     
 @st.cache(show_spinner=False)
 def load_model():
@@ -36,11 +71,8 @@ def application():
     record = st.button('Record Now')
     
     if record:
-        myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-        sd.wait()  # Wait until recording is finished
-        write('input.wav', fs, myrecording)  # Save as WAV file 
+        record_audio()
         st.success('Emotion of the audio is  ',predict(model,'input.wav'))
-        
 
 def extract_mfcc(wav_file_name):
     #This function extracts mfcc features and obtain the mean of each dimension
