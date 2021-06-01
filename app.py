e0b2c51e7881e0476df5efc23e02bfe0936ee701
sunilbelde <47601858+sunilbelde@users.noreply.github.com>
2021-06-01 18:04:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,43 +6,6 @@ import pyaudio
 import wave
 
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 3.5
-WAVE_OUTPUT_FILENAME = "input.wav"
-
-p = pyaudio.PyAudio()
-
-
-def record_audio():
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-    print("* recording")
-
-    frames = []
-
-    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-        data = stream.read(CHUNK)
-        frames.append(data)
-
-    print("* done recording")
-
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
-
-    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-    wf.setnchannels(CHANNELS)
-    wf.setsampwidth(p.get_sample_size(FORMAT))
-    wf.setframerate(RATE)
-    wf.writeframes(b''.join(frames))
-    wf.close()
 def main():
     #print(cv2.__version__)
     selected_box = st.sidebar.selectbox(
@@ -68,10 +31,9 @@ def application():
     fs = 44100  # Sample rate
     seconds = 3  # Duration of recording
     
-    record = st.button('Record Now')
+    file_to_be_uploaded = st.file_uploader("Choose an audio...", type="wav")
     
-    if record:
-        record_audio()
+    if file_to_be_uploaded:
         st.success('Emotion of the audio is  ',predict(model,'input.wav'))
 
 def extract_mfcc(wav_file_name):
